2c1f8d70af1e064de9b9d06df491ee456c4bec6d
zipu888 <zipu405@126.com>
2015-04-30 12:13:55 +0800

    modify

--- src/main/java/com/xiaohao/neo4j/sample/controller/MovieController.java
+++ src/main/java/com/xiaohao/neo4j/sample/controller/MovieController.java
@@ -1,11 +1,21 @@
 package com.xiaohao.neo4j.sample.controller;
 
+import com.xiaohao.neo4j.sample.service.MovieService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
  * Created by Administrator on 15-4-28.
  */
 @Controller
+@RequestMapping("/")
 public class MovieController {
+    @Autowired
+    MovieService movieService;
 
+    @RequestMapping("test")
+    public String test(){
+        return null;
+    }
 }
--- src/main/java/com/xiaohao/neo4j/sample/service/MovieService.java
+++ src/main/java/com/xiaohao/neo4j/sample/service/MovieService.java
@@ -3,14 +3,17 @@ package com.xiaohao.neo4j.sample.service;
 import com.xiaohao.neo4j.sample.model.Movie;
 import com.xiaohao.neo4j.sample.repository.MovieRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 /**
  * Created by Administrator on 15-4-28.
  */
+@Service
 public class MovieService {
 
     @Autowired
     MovieRepository movieRepository;
+
 
     public void test(){
         movieRepository.save(new Movie());
